5d3b0fc5aa2da55d3260bd3ccc02e7dab2b437cb
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-09-21 11:46:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def all_cars():
 
 # @app.route('/reservation', methods=['GET', 'POST'])
 @app.route('/reservation/<int:car_id>', methods=['GET', 'POST'])
-def reserve(car_id=none):
+def reserve(car_id):
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
