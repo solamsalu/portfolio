d8fa78e2f430e9f91c032319c5dc4f3691a6ce4b
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-09-21 23:50:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ db = SQLAlchemy(app)
 #     remember_me = BooleanField('Remember Me')
 #     submit = SubmitField('Login')
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(100), nullable=False)
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(50), unique=True, nullable=False)
+#     email = db.Column(db.String(50), unique=True, nullable=False)
+#     password_hash = db.Column(db.String(100), nullable=False)
     
 class CarList(db.Model):
     __tablename__ = 'CarList'
